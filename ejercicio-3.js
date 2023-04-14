ba4b73573c2636aed80e1caa22872a532ff29deb
Hector Salguero <78835095+1andres5s@users.noreply.github.com>
2023-04-13 20:29:54 -0500

    ejercicio con constantes

---
+++ ejercicio-3.js
@@ -0,0 +1,11 @@
+const PI = 3.1416
+
+
+console.log('el valor de pu es: ' + PI)	
+
+const segundos = 60
+const minutos = 60
+const hora = 24
+
+
+console.log('un minuto tienes ' + segundos + "  segundos. una hora tiene " + minutos + " minutos ")
