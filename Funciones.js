b9212acb2f8cce7fe5e983e9ee7f92750c86df3c
Hector Salguero <78835095+1andres5s@users.noreply.github.com>
2023-04-16 22:10:07 -0500

    maaaaaaaaaaas ejercicios

---
+++ Funciones.js
@@ -0,0 +1,23 @@
+let n1= 14 
+let n2 = 12
+
+function sumar (a,b){
+    return a+b;
+}
+console.log(sumar(n1,n2))
+let num = sumar(n1,n2)
+console.log(num)
+
+
+function saludar(){
+    msj = "hola mundo";
+    return msj
+}
+
+console.log(saludar());
+
+
+const saludo = function(){
+    mssg = "holis mundo";
+    return mssg;
+}
